1110befc1e6558c4a5dcc7876a415b636758eae0
Usohatchi <eamon@itofisher.com>
2019-03-23 12:47:13 -0700

    Updated train files

--- src/train/train_a3c.py
+++ src/train/train_a3c.py
@@ -60,7 +60,8 @@ def main(args,
                                summary_writer=summary_writer,
                                save_path=save_dir,
                                visual_path=args.visual_dir,
-                               visual_period=visual_period)
+                               visual_period=visual_period,
+                               load_path=args.restore)
 
     
     if args.human_input != None:
@@ -89,6 +90,10 @@ if __name__ == '__main__':
         default=None)
     parser.add_argument(
         '--visual-dir',
+        type=str,
+        default=None)
+    parser.add_argument(
+        '--restore',
         type=str,
         default=None)
     parser.add_argument(
