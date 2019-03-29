82eaadea3d4dd55778d69d3ff605d8a8729eae08
Oleksandra Fedorova <emoilenne@gmail.com>
2019-03-28 23:39:24 -0700

    add actions

--- src/misc/extract_embeddings.py
+++ src/misc/extract_embeddings.py
@@ -3,7 +3,35 @@ import pickle
 import numpy as np
 import os
 
-def extract_paired_embeddings(memory_filename, output_filename, frames_into_future):
+def extract_embeddings_actions(memory_filename, output_filename):
+    with open(memory_filename, 'rb') as mf:
+        memory = pickle.load(mf)
+
+    embeddings = []
+    actions = []
+
+    if type(memory) == type([]) and hasattr(memory[0], 'states'):
+        for m in memory:
+            if len(m.states) == 0: continue
+            embeddings.append(np.array([np.array(s) for s in m.states]))
+            actions.append(np.array(m.actions))
+    elif hasattr(memory, 'states'):
+        embeddings.append(np.array([np.array(s) for s in memory.states]))
+        actions.append(np.array(memory.actions))
+    else:
+        raise Exception('Provide human replay or memory file as input.')
+
+    print('Embeddings shapes: {}'.format(', '.join([str(ems.shape) for ems in embeddings])))
+    print('Actions shapes: {}'.format(', '.join([str(acts.shape) for acts in actions])))
+    print('Saving dictionary with keys "embeddings", "actions"')
+
+    os.makedirs(os.path.dirname(output_filename), exist_ok=True)
+    with open(output_filename, 'wb') as of:
+        pickle.dump({'embeddings': embeddings, 'actions': actions}, of)
+    print('Saved!')
+
+
+def extract_paired_embeddings(memory_filename, output_filename, frames):
     with open(memory_filename, 'rb') as mf:
         memory = pickle.load(mf)
 
@@ -12,14 +40,14 @@ def extract_paired_embeddings(memory_filename, output_filename, frames_into_futu
     if type(memory) == type([]) and hasattr(memory[0], 'states'):
         for m in memory:
             embeddings = [np.array(s) for s in m.states]
-            if frames_into_future > 0:
-                paired_embeddings.extend(list(zip(embeddings[:-frames_into_future], embeddings[frames_into_future:])))
+            if frames > 0:
+                paired_embeddings.extend(list(zip(embeddings[:-frames], embeddings[frames:])))
             else:
                 paired_embeddings.extend(list(zip(embeddings, embeddings)))
     elif hasattr(memory, 'states'):
         embeddings = [np.array(s) for s in memory.states]
-        if frames_into_future > 0:
-            paired_embeddings = list(zip(embeddings[:-frames_into_future], embeddings[frames_into_future:]))
+        if frames > 0:
+            paired_embeddings = list(zip(embeddings[:-frames], embeddings[frames:]))
         else:
             paired_embeddings = list(zip(embeddings, embeddings))
     else:
@@ -75,13 +103,16 @@ if __name__ == '__main__':
     parser.add_argument('--output', type=str, default='./embeddings/extracted')
     parser.add_argument('--memory', type=str, required=True)
     parser.add_argument('--paired', action='store_true')
-    parser.add_argument('--frames-into-future', type=int, default=1)
+    parser.add_argument('--actions', action='store_true')
+    parser.add_argument('--frames', type=int, default=1, help='frames into future for paired embeddings')
     args = parser.parse_args()
 
     memory_filename = args.memory
     output_filename = args.output
-    frames_into_future = args.frames_into_future
-    if args.paired:
-        extract_paired_embeddings(memory_filename, output_filename, frames_into_future)
+    frames = args.frames
+    if args.actions:
+        extract_embeddings_actions(memory_filename, output_filename)
+    elif args.paired:
+        extract_paired_embeddings(memory_filename, output_filename, frames)
     else:
         extract_embeddings(memory_filename, output_filename)
